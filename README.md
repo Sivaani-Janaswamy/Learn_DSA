103ab755700da34125c9f8e7e57fb3d8dfea13ad
Sivaani <sivaanijanaswamy@gmail.com>
2025-01-28 22:02:05 +0530

    Organized files, added SLL.py, and updated README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-DSA Practice Files
+Data Structures and Algorithms
+
+This repo will help you get started with data structures and algorithms for complete beginners. Topics folder contains basic level implementations of common data structures like Linked List - Singly Linked List, Doubly Linked List,.., Stacks, etc.. and also common algorithms. Ds Lab contains easy level problems to understand the data structures better. Other folders contain solutions to Leetcode, Hackerrank, Hackerrearth, etc.. problems. 
+
---
+++ topics/SLL.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+class SLL:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBegin(self,ele):
+        newnode = Node(ele)
+        newnode.next = self.head
+        self.head = newnode 
+        
+    def insertAtEnd(self,ele):
+        newnode = Node(ele)
+        if self.head is None:
+            self.head = newnode
+            return
+        temp = self.head
+        while(temp.next):
+            temp = temp.next
+        temp.next = newnode
+    
+    def insertAtPos(self,ele,pos):
+        if pos < 0:
+            print("Invalid position!")
+            return
+        newnode = Node(ele)
+        if pos == 0:  
+            newnode.next = self.head
+            self.head = newnode
+            return
+        temp = self.head
+        i = 0
+        while temp is not None and i < pos - 1:
+            temp = temp.next
+            i += 1
+        if temp is None:
+            print("Position out of bounds!")
+            return
+        newnode.next = temp.next
+        temp.next = newnode
+
+    def traversal(self):
+        temp = self.head
+        while(temp):
+            print(temp.val,end=" -> ")
+            temp = temp.next
+        print("NULL\n")
+    
+slist = SLL()
+slist.insertAtBegin(10)
+slist.insertAtEnd(20)
+slist.insertAtPos(40,2)
+slist.traversal()
