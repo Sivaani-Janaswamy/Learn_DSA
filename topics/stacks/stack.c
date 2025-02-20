038d35b8519668ba5fc73bb1734158bd221de4d3
Sivaani <sivaanijanaswamy@gmail.com>
2025-02-20 20:49:11 +0530

    all

---
+++ topics/stacks/stack.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+#define MAX = 10;
+bool isFull(int top){
+    if(top == MAX){
+       return true;
+    }
+    return false;
+}
+bool isEmpty(int top){
+    if(top == -1){
+        return true;
+    }
+    return false;
+}
+void push(int *top, int stacks[], int ele){
+    if()
+}
--- topics/tree/binary_tree.py
+++ topics/tree/binary_tree.py
@@ -6,12 +6,38 @@ class Node:
 class Tree:
     def __init__(self):
         self.root = None
-    def inorder(self):
-        if self.root is None:
+    def inorder(self, node):
+        if node is None:
             return
-        inorder(self.root.left)
-        print(self.root.data)
-        inorder(self.root.right)
+        self.inorder(node.left)
+        print(node.data, end = " ")
+        self.inorder(node.right)
+    def preorder(self, node):
+        if node is None:
+            return
+        print(node.data, end = " ")
+        self.preorder(node.left)
+        self.preorder(node.right)
+    def postorder(self, node):
+        if node is None:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        print(node.data, end = " ")
 
+tree = Tree()
+tree = Tree()
+tree.root = Node(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+print("\nInorder: ")
+tree.inorder(tree.root)
+print("\nPreorder: ")
+tree.preorder(tree.root)
+print("\nPostorder: ")
+tree.postorder(tree.root)
 
   
